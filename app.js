582b17816990a4961e784dca2c53393a6dd310e9
Cygnut <lee.crosby@outlook.com>
2016-12-06 22:35:36 +0000

    =Move from svn to git

---
+++ app.js
@@ -0,0 +1,53 @@
+var program = require('commander');
+var e = require('./encrypt');
+
+program
+  .version('0.0.1')
+ 
+program
+  .command('encrypt')
+  .alias('e')
+  .description('encrypt the file with the given password')
+  .option("-p, --password <password>", "the password to use")
+  .option("-i, --input <input>", "the path to the input file")
+  .option("-o, --output [output]", "the path to the output file. If not specified, is input plus '.encrypted'")
+  .action(function(options) {
+    options = options || {}
+    
+    if (!options.password)
+        throw new Error("password required.");
+    
+    if (!options.input)
+        throw new Error("input filepath required.");
+    
+    var output = options.output || options.input + '.encrypted';
+    
+    e.encrypt(options.password, options.input, output);
+    
+    console.log('password %s, input %s, output %s', options.password, options.input, output);
+  });
+ 
+program
+  .command('decrypt')
+  .alias('d')
+  .description('decrypt the file with the given password')
+  .option("-p, --password <password>", "the password to use")
+  .option("-i, --input <input>", "the path to the input file")
+  .option("-o, --output [output]", "the path to the output file. If not specified, is input plus '.decrypted'")
+  .action(function(options) {
+    options = options || {}
+    
+    if (!options.password)
+        throw new Error("password required.");
+    
+    if (!options.input)
+        throw new Error("input filepath required.");
+    
+    var output = options.output || options.input + '.decrypted';
+    
+    e.decrypt(options.password, options.input, output);
+    
+    console.log('password %s, input %s, output %s', options.password, options.input, output);	
+  });
+
+program.parse(process.argv);
---
+++ encrypt.js
@@ -0,0 +1,30 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+function convert(password, input, output, encrypt)
+{
+	const algorithm = 'aes192';
+	const file_encoding = 'utf8';
+	const output_encoding = 'hex';
+	
+	
+	var crypt = 
+		encrypt 
+		?
+		crypto.createCipher(algorithm, password)
+		:
+		crypto.createDecipher(algorithm, password);
+	
+	const inputStream = fs.createReadStream(input);
+	const outputStream = fs.createWriteStream(output);
+	
+	inputStream.pipe(crypt).pipe(outputStream);
+}
+
+module.exports.encrypt = function(password, input, output) {
+	convert(password, input, output, true);
+}
+
+module.exports.decrypt = function(password, input, output) {
+	convert(password, input, output, false);
+}
